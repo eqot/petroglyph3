182b1166812d51cc655db45baab5a5604ea02b52
Ikuo Terado <eqobar@gmail.com>
2013-11-20 02:23:35 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Petroglyph::Application.config.secret_key_base = '9ba6e8c16e1ebafb5835105d3fa4bf8c0deae07470f827f4f9b8069ea83cbd159aba90bf6413e7645929814f5c09511972b94e82ab6e1854d26ee73d7a206d1d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Petroglyph::Application.config.session_store :cookie_store, key: '_petroglyph_session'
