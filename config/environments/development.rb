bd5e0d02a67a57bb7e04349a5b134062b31dd154
Ikuo Terado <eqobar@gmail.com>
2013-11-20 23:01:51 +0900

    Add settings and views for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,4 +29,7 @@ Petroglyph::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Settings for devise.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
