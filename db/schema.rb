e494300613bc069ed93ab53ed6d6274a59bbb15f
Ikuo Terado <eqobar@gmail.com>
2013-11-22 01:08:52 +0900

    Add video add view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131121150539) do
+ActiveRecord::Schema.define(version: 20131121160627) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20131121150539) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "duration"
   end
 
 end
