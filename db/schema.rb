25b34f26ee6a2da7bc5d0e177145ec199d2f6bc4
Ikuo Terado <eqobar@gmail.com>
2013-11-23 08:32:53 +0900

    Add admin flag to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131122223607) do
+ActiveRecord::Schema.define(version: 20131122233008) do
 
   create_table "contains", force: true do |t|
     t.integer  "video_id"
@@ -35,12 +35,12 @@ ActiveRecord::Schema.define(version: 20131122223607) do
   add_index "playlists", ["user_id", "created_at"], name: "index_playlists_on_user_id_and_created_at"
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20131122223607) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
