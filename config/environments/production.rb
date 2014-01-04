587506bfce5b3836b98bfc933ccb399dae6b1f8f
Ikuo Terado <eqobar@gmail.com>
2014-01-04 22:39:02 +0900

    Minor fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Petroglyph::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Settings for devise.
+  config.action_mailer.default_url_options = { :host => 'petroglyph.eqot.net' }
 end
