dc2b5c29db732ff114d96d8b36ec30e0956977ba
Ikuo Terado <eqobar@gmail.com>
2014-01-05 00:05:03 +0900

    Add fonts path to assets paths

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module Petroglyph
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.enforce_available_locales = true
+
+    # Add fonts path which includes Bootstrap's glyphicons
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
