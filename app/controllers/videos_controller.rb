945dc0b47bf8f4024587532d125f3b4f90474364
Ikuo Terado <eqobar@gmail.com>
2014-01-19 19:05:04 +0900

    Avoid viewing videos and playlists by unauthorized user

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,5 @@
 class VideosController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :destroy]
+  before_action :authenticate_user!
   before_action :get_video, only: [:show, :edit, :update, :destroy]
 
   def new
